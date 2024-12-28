1b13d59eed73e37145b22c65c41a60dbee12d05c
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2024-12-28 14:23:13 +0000

    Initial commit from Codespaces

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import ApplyJob from './pages/ApplyJob'
+import Applications from './pages/Applications'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/apply-job/:id" element={<ApplyJob />} />
+      <Route path="/applications" element={<Applications />} />
+    </Routes>
   )
 }
 
