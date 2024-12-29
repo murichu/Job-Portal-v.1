a44f9d6f15704d4fb6dbd05c95573878eb29b16d
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2024-12-29 09:39:14 +0000

    hero update

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,21 +2,36 @@ import React from 'react';
 import { assets } from '../assets/assets'; 
 
 const Hero = () => {
+  
   return (
     <div className="container 2xl:px-20 mx-auto my-10">
       <div className="bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl">
         <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 10,000+ jobs to apply</h2>
         <p className='mb-8 max-w-xl mx-auto text-sm font-light px-5'>Your Next Big Career Move Starts Right Here - Explore the Best Job Opportunities and Take the First Step Toward Your Future!</p>
-        <div>
         <div className="flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto">
-          <img src={assets.search_icon} alt="" />
-          <input type='text' placeholder='Search for jobs' className='max-sm:text-xs p-2 rounded outline-none w-full' />
+          <div className="flex items-center">
+            <img className='h-4 sm:h-5' src={assets.search_icon} alt="Search icon" />
+            <input id="job-search" type='text' placeholder='Search for jobs' className='max-sm:text-xs p-2 rounded outline-none w-full' />
+          </div>
+          <div className="flex items-center">
+            <img className='h-4 sm:h-5' src={assets.location_icon} alt="Location icon" />
+            <input id="location" type='text' placeholder='Location' className='max-sm:text-xs p-2 rounded outline-none w-full' />
+          </div>
+          <button className='bg-blue-600 px-6 py-2 rounded text-white m-1'>
+            Search
+          </button>
         </div>
-        <div className="search-container">
-          <img src={assets.location_icon} alt="" />
-          <input type='text' placeholder='Location' className='max-sm:text-xs p-2 rounded outline-none w-full' />
-        </div>
-        <button>Search</button>
+      </div>
+
+      <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
+        <div className='flex justify-center gap-10 lg:gap-16 flex-wrap'>
+          <p className='font-medium'>Trusted by</p>
+          <img className='h-6' src={assets.microsoft_logo} alt="Microsoft logo" />
+          <img className='h-6' src={assets.walmart_logo} alt="Walmart logo" />
+          <img className='h-6' src={assets.accenture_logo} alt="Accenture logo" />
+          <img className='h-6' src={assets.samsung_logo} alt="Samsung logo" />
+          <img className='h-6' src={assets.amazon_logo} alt="Amazon logo" />
+          <img className='h-6' src={assets.adobe_logo} alt="Adobe logo" />
         </div>
       </div>
     </div>
