7c8dd60ed15a695cf9a18b89222fe62d81a301b6
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2024-12-29 10:41:57 +0000

    search bt

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
 import { assets } from '../assets/assets'; 
+import { AppContext } from '../context/AppContext'
 
 const Hero = () => {
-  
+
+  const { setSearchFilter, setIsSearched } = useContext(AppContext)
+
+  const titleRef = useRef(null)
+  const locationRef = useRef(null)
+
+  const onSearch = () => {
+    setSearchFilter ({
+      title: titleRef.current.value,
+      location: locationRef.current.value
+    })
+    setIsSearched(true)
+    console.log({
+      title: titleRef.current.value,
+      location: locationRef.current.value
+    });
+    
+  }
+
   return (
     <div className="container 2xl:px-20 mx-auto my-10">
       <div className="bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl">
@@ -11,13 +30,15 @@ const Hero = () => {
         <div className="flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto">
           <div className="flex items-center">
             <img className='h-4 sm:h-5' src={assets.search_icon} alt="Search icon" />
-            <input id="job-search" type='text' placeholder='Search for jobs' className='max-sm:text-xs p-2 rounded outline-none w-full' />
+            <input id="job-search" type='text' placeholder='Search for jobs' className='max-sm:text-xs p-2 rounded outline-none w-full' 
+            ref={titleRef} />
           </div>
           <div className="flex items-center">
             <img className='h-4 sm:h-5' src={assets.location_icon} alt="Location icon" />
-            <input id="location" type='text' placeholder='Location' className='max-sm:text-xs p-2 rounded outline-none w-full' />
+            <input id="location" type='text' placeholder='Location' className='max-sm:text-xs p-2 rounded outline-none w-full'
+            ref={locationRef} />
           </div>
-          <button className='bg-blue-600 px-6 py-2 rounded text-white m-1'>
+          <button onClick={onSearch} className='bg-blue-600 px-6 py-2 rounded text-white m-1'>
             Search
           </button>
         </div>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,15 +1,25 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+    const [searchFilter, setSearchFilter] = useState({
+        title: '',
+        location: ''
+    });
+
+    const [isSearched, setIsSearched] = useState(false);
+
     const value = {
-        // Add any values or functions you want to provide here
-    };
+        searchFilter,
+        setSearchFilter,
+        isSearched,
+        setIsSearched,
+    }
 
     return (
         <AppContext.Provider value={value}>
-            {props.children}
+            {props.children} 
         </AppContext.Provider>
     );
 };
