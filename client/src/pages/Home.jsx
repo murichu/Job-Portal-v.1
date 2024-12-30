83f65f5293924d6d19830a03bf19870edac51250
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2024-12-30 12:27:29 +0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import JobListings from '../components/JobListings'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <JobListings />
     </div>
   )
 }
