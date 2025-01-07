bb94b8e5c671806312f34f727177d3a63da2bc85
AnisaJacob <anisa.muthoni2020@gmail.com>
2025-01-07 07:38:25 +0000

    fix app.js and Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import ApplyJob from "./pages/ApplyJob";
 import Applications from "./pages/Applications";
 import RecruiterLogin from "./components/RecruiterLogin";
 import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplications from "./pages/ViewApplications";
 
 const App = () => {
   const { showRecruiterLogin } = useContext(AppContext);
@@ -16,6 +20,14 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
+
+        {/* Parent route for the Dashboard */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          {/* Nested routes under /dashboard */}
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+        </Route>
       </Routes>
     </div>
   );
