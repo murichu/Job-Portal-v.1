4ed4cc87df6a4f5df3ef6ed10e85200836a2de69
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2025-01-17 18:10:46 +0300

    Update userRouter.js

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, logout, sendVerifyOtp, verifyEmail, isAuthenticated } from '../controllers/userController.js';
+import { register, login, logout, sendVerifyOtp, verifyEmail, isAuthenticated, sendResetOtp, resetPassword } from '../controllers/userController.js';
 import userAuth from '../middleware/userAuth.js';
 
 
@@ -11,6 +11,8 @@ userRouter.post('/logout', logout);
 userRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
 userRouter.post('/verify-account', userAuth, verifyEmail);
 userRouter.post('/is-auth', userAuth, isAuthenticated);
+userRouter.post('/send-reset-otp', sendResetOtp);
+userRouter.post('/reset-password', resetPassword);
 
 
 export default userRouter; 
