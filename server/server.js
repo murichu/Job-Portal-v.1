3fa7322624f6ca5001ec74b92d4542b98c48451b
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2025-07-18 02:37:38 +0000

    added clerk auth

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ import * as Sentry from "@sentry/node";
 import connectDB from "./config/mongoDB.js";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/userRouter.js";
-import { clerkWebhooks } from "./controllers/webhooks.js";
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 
 // Initialize dotenv to load environment variables
 dotenv.config();
@@ -21,11 +24,14 @@ connectDB();
 app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(clerkMiddleware())
 
 // Routes
 app.get("/", (req, res) => {
   res.send("API is working!");
 });
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // API Endpoints
 app.use("/api/user", userRouter);
@@ -34,7 +40,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 
-app.post("/webhooks", clerkWebhooks);
+
 
 // Port
 const PORT = process.env.PORT || 5000;
